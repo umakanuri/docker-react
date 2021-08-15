2127ccfab223b3a8357638f4050f982f0150eecf
Uma Kanuri <uma.kanuri11@gmail.com>
2021-08-14 20:12:14 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Uma! This is from Windows with docker volume 
+          Hi Uma! This is from AWS from featuer branch. 
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
